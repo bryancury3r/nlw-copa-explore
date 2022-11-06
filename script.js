7f7dfc74650d1a3575f312303f83d976a2e68de3
bryancury3r <gabriel_bryancury@outlook.com>
2022-11-06 15:38:25 -0300

    criando function e style

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-document.querySelector('#app').innerHtml = `
-<header>
-<img src="./assets/logo.svg" alt="logo">
-</header>
-<main id="cards">
-<div class="card">
-<h2>24/11 <span>quinta</span></h2>
-<ul>
-  <li>
-    <img src="./assets/icon-brazil.svg" alt="Bandeira do Brazil">
-    <strong>16:00</strong>
-    <img src="./assets/icon-serbia.svg" alt="Bandeira da Serbia">
-  </li>
+function createCard() {
+  return `
+        <div class="card">
+          <h2>24/11 <span>quinta</span></h2>
+          <ul>
+            <li>
+              <img src="./assets/icon-brazil.svg" alt="Bandeira do Brazil">
+              <strong>16:00</strong>
+              <img src="./assets/icon-serbia.svg" alt="Bandeira da Serbia">
+            </li>
 
-  <li>
-    <img src="./assets/icon-brazil.svg" alt="Bandeira do Brazil">
-    <strong>08:00</strong>
-    <img src="./assets/icon-korea.svg" alt="Bandeira da Serbia">
-  </li>
-</ul>
-</div>
+            <li>
+              <img src="./assets/icon-brazil.svg" alt="Bandeira do Brazil">
+              <strong>08:00</strong>
+              <img src="./assets/icon-korea.svg" alt="Bandeira da Serbia">
+            </li>
+          </ul>
+        </div>
+  `
+}
+
+document.querySelector("#app").innerHTML = `
+      <header>
+          <img src="./assets/logo.svg" alt="logo">
+      </header>
+      <main id="cards">
+        ${createCard()}
+        ${createCard()}
+
+      </main>
 `
