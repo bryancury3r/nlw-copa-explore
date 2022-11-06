ffe3d2269723e3d94a77ab93138eba5ebba47f62
bryancury3r <gabriel_bryancury@outlook.com>
2022-11-06 15:28:29 -0300

    add script

---
+++ script.js
@@ -0,0 +1,22 @@
+document.querySelector('#app').innerHtml = `
+<header>
+<img src="./assets/logo.svg" alt="logo">
+</header>
+<main id="cards">
+<div class="card">
+<h2>24/11 <span>quinta</span></h2>
+<ul>
+  <li>
+    <img src="./assets/icon-brazil.svg" alt="Bandeira do Brazil">
+    <strong>16:00</strong>
+    <img src="./assets/icon-serbia.svg" alt="Bandeira da Serbia">
+  </li>
+
+  <li>
+    <img src="./assets/icon-brazil.svg" alt="Bandeira do Brazil">
+    <strong>08:00</strong>
+    <img src="./assets/icon-korea.svg" alt="Bandeira da Serbia">
+  </li>
+</ul>
+</div>
+`
